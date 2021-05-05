4b7d18318d3d569ebc1da492aef9ab53b7a0cd13
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-05-06 02:16:40 +0300

    add Mapper

---
+++ TaskSheduler.BusinessLayer/DTO/UserTaskDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSheduler.BusinessLayer.DTO
+{
+    class UserTaskDTO
+    {
+        public int Id { get; set; }
+        public string NameOfTask { get; set; }
+        public string Description { get; set; }
+        public DateTime StartOfTask { get; set; }
+        public DateTime DeadLine { get; set; }
+        public string Status { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ TaskSheduler.BusinessLayer/Mapping/UserMapperProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSheduler.BusinessLayer.Mapping
+{
+    public class UserMapperProfile : Profile
+    {
+        public UserMapperProfile()
+        {
+            CreateMap<User, UserDTO>()
+            .ForMember(dst => dst.Name, opts => opts.MapFrom(src => src.FirstName + " " + src.SecondName))
+            .ForMember(dst => dst.Rule, opts => opts.MapFrom(src => src.Rule));
+                //.ReverseMap()
+                //.ForMember(dst => dst.)
+        }
+    }
+}
